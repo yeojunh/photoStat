de89b67df5d55c40f52a1b3b6b7984ed9999b460
Yeojun Han <yeojunhann@gmail.com>
2022-05-03 19:55:37 -0700

    allow numerical words to represent numbers

--- stat.py
+++ stat.py
@@ -1,6 +1,7 @@
 import scipy.stats as ss
 import numpy as np
 import re
+from word2number import w2n
 
 #public variables 
 x = 0
@@ -14,7 +15,6 @@ two_tailed = False
 phrase1 = "The mean entry level salary of an employee at a company is $58,000. You believe it is higher for IT professionals in the company. State the null and alternative hypotheses."
 phrase2 = "You are testing that the mean speed of your cable Internet connection is more than three Megabits per second. State the null and alternative hypotheses."
 phrase3 = "A sociologist claims the probability that a person picked at random in Times Square in New York City is visiting the area is 0.83. You want to test to see if the claim is correct. State the null and alternative hypotheses."
-
 
 def z_test(p, x, sigma, n, alpha, two_tailed): 
     stdErr = sigma/np.sqrt(n)
@@ -47,13 +47,12 @@ def find_hypothesis(problem):
 def find_test_val(problem):
     test_val = 0
     # val_equal = True
-    extract_nums = [int(s) for s in problem.split() if s.isdigit()]
 
     nums = re.findall('\d+[,\.]?\d*', problem)
     # print(nums)
     if len(nums) == 1: 
         return nums[0]
-    else: 
+    elif len(nums) > 1: 
         nonYears = set()
         for i in range(len(nums)):
             if re.search('[12]\d\d\d', nums[i]) == None: 
@@ -64,6 +63,9 @@ def find_test_val(problem):
             return nonYears[0]
         else: # more than 1 non years 
             return nonYears[0] # multiple values that do not appear to be non-years. just use the first value. TODO: handle this better
+    else: 
+        word_num = w2n.word_to_num(problem)
+        return str(word_num)
             
 
 
@@ -112,5 +114,11 @@ def test_find_hypothesis():
 def test_find_test_val(): 
     result = find_test_val(phrase1)
     assert result == "58,000"
+    
+    result = find_test_val(phrase2)
+    assert result == "3"
+
+    result = find_test_val(phrase3)
+    assert result == "0.83"
 
 test_find_test_val()
